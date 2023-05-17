e58e8c37275718e0777809f4698c6c9ef50d4d16
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-05-17 15:03:56 +0900

    mypage 수정기능 구현

--- src/component/Body/mypage_page/Mypage.jsx
+++ src/component/Body/mypage_page/Mypage.jsx
@@ -2,55 +2,147 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import "./mypage.css";
+import axios from "axios";
 function Mypage({ user }) {
+  const [isEdit, setIsEdit] = useState(false);
+  const [password, setPassword] = useState("");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [editUser, setEditUser] = useState({
+    name: user?.name,
+    email: user?.eamil,
+    phoneNumber: user?.phoneNumber,
+  });
+  const handleEditUserChange = (e) => {
+    setEditUser({
+      ...editUser,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleEditUserSubmit = (e) => {
+    e.preventDefault();
+    console.log(editUser);
+    axios
+      .post("http://localhost:4000/user/edit", { _id: user?._id, editUser })
+      .then((res) => {
+        console.log("수정중");
+        if (res.status === 204) {
+          console.log("수정완료");
+          alert("수정완료");
+          window.location.href = "/";
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    //여기서 수정 요청보내기
+  };
+  const handleEditing = () => {
+    setIsEdit((prev) => !prev);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const handlePasswordSubmit = (e) => {
+    e.preventDefault();
+    if (checkPassword(password)) {
+      setIsAuthenticated(true);
+    } else {
+      alert("비밀번호가 잘못되었습니다.");
+    }
+  };
+  const checkPassword = (password) => {
+    return password === user?.password;
+  };
+
   if (!localStorage.getItem("LoginUser")) {
     return <h1>잘못된 접근, Login후 접속가능</h1>;
   }
 
   return (
     <>
-      <h1>myPage입니다</h1>
-      <div className="container">
-        <div className="item">
-          <span>학번 : </span>
-          <input value={user?.studentNumber || ""} disabled></input>
-        </div>
-        <div className="item">
-          <span>username : </span>
-          <input value={user?.name || ""} disabled></input>
-        </div>
-        <div className="item">
-          <span>이메일 : </span>
-          <input value={user?.email || ""} disabled></input>
-        </div>
-        <div className="item">
-          <span>전화번호 : </span>
-          <input value={user?.phoneNumber || ""} disabled></input>
-        </div>
-        <div className="item">
-          <span>회원구분 : </span>
-          {user?.adminStatus ? (
-            <select defaultValue="일반회원">
-              <option value="임원" disabled>
-                임원
-              </option>
-              <option value="일반회원" disabled>
-                일반회원
-              </option>
-            </select>
-          ) : (
-            <select defaultValue="임원">
-              <option disabled>임원</option>
-              <option disabled>일반회원</option>
-            </select>
-          )}
-        </div>
-      </div>
-      {/* {user?.adminStatus && <h3>회원구분 : 임원</h3>}
-      {!user?.adminStatus && <h3>회원구분 : 일반회원</h3>} */}
-      <Button variant="primary" size="sm">
-        수정
-      </Button>{" "}
+      {!isAuthenticated ? (
+        <form onSubmit={handlePasswordSubmit}>
+          <label>
+            비밀번호를 입력하시오:
+            <input
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </label>
+          <button type="submit">확인</button>
+        </form>
+      ) : (
+        <>
+          <h1>myPage입니다</h1>
+          <form onSubmit={handleEditUserSubmit} className="container">
+            <label className="item">
+              <span>학번 : </span>
+              <input
+                name="studentNumber"
+                placeholder={user?.studentNumber || ""}
+                disabled
+              ></input>
+            </label>
+            <label className="item">
+              <span>username : </span>
+              <input
+                name="name"
+                placeholder={user?.name || ""}
+                disabled={!isEdit}
+                onChange={handleEditUserChange}
+              ></input>
+            </label>
+            <label className="item">
+              <span>이메일 : </span>
+              <input
+                name="email"
+                placeholder={user?.email || ""}
+                disabled={!isEdit}
+                onChange={handleEditUserChange}
+              ></input>
+            </label>
+            <label className="item">
+              <span>전화번호 : </span>
+              <input
+                name="phoneNumber"
+                placeholder={user?.phoneNumber || ""}
+                disabled={!isEdit}
+                onChange={handleEditUserChange}
+              ></input>
+            </label>
+            <label className="item">
+              <span>회원구분 : </span>
+              {user?.adminStatus ? (
+                <select>
+                  <option selected disabled>
+                    임원
+                  </option>
+                  <option disabled>일반회원</option>
+                </select>
+              ) : (
+                <select>
+                  <option disabled>임원</option>
+                  <option selected disabled>
+                    일반회원
+                  </option>
+                </select>
+              )}
+            </label>
+
+            {!isEdit && (
+              <Button onClick={handleEditing} variant="primary" size="sm">
+                수정
+              </Button>
+            )}
+            {isEdit && (
+              <Button type="submit" variant="primary" size="sm">
+                저장
+              </Button>
+            )}
+          </form>
+        </>
+      )}
     </>
   );
 }
